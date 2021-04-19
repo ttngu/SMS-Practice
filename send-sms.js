1405f8d8c6ab6d44d65fb6d31308e7540493b56c
ttngu <tammyn137@gmail.com>
2021-04-19 01:45:39 -0700

    added array of phone numbers

--- send-sms.js
+++ send-sms.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-
 // Create Twilio REST Client
-const client = require('twilio')(accountSid,authToken);
+const client = require('twilio')(accountSid,authToken)
+
+// Array of phone numbers
+const phoneNumbers = ["+1111-222-3333", "+1222-333-4444", "+1333-444-5555", "+1123-456-7890", "+1987-654-3210"];
 
 client.messages.create({
     to: process.env.MY_PHONENUMBER,
