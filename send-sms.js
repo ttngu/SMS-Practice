9c49f30d6cf2775d821772285569d561a62ccd74
ttngu <tammyn137@gmail.com>
2021-04-19 02:20:52 -0700

    loop logs each number

--- send-sms.js
+++ send-sms.js
@@ -8,10 +8,14 @@ const client = require('twilio')(accountSid,authToken)
 // Array of phone numbers
 const phoneNumbers = ["+1111-222-3333", "+1222-333-4444", "+1333-444-5555", "+1123-456-7890", "+1987-654-3210"];
 
-client.messages.create({
-    to: process.env.MY_PHONENUMBER,
-    from: process.env.TWILIO_NUMBER,
-    body: 'Hello there! This is Potato.'
-})
-.then ((message) => console.log(message.sid))
-.catch((err) => console.log(err));
+phoneNumbers.forEach( phoneNumbers => console.log(phoneNumbers));
+
+
+
+// client.messages.create({
+//     to: process.env.MY_PHONENUMBER,
+//     from: process.env.TWILIO_NUMBER,
+//     body: 'Hello there! This is Potato.'
+// })
+// .then ((message) => console.log(message.sid))
+// .catch((err) => console.log(err));
