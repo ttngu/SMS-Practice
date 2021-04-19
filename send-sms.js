3e97aa48759687cdfc67321af9b5dac67ef4ce6f
ttngu <tammyn137@gmail.com>
2021-04-19 02:44:32 -0700

    removed country code from array and added it to the create, tested and works properly

--- send-sms.js
+++ send-sms.js
@@ -1,3 +1,5 @@
+const twilio = require('twilio');
+
 require('dotenv').config()
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -6,9 +8,20 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid,authToken)
 
 // Array of phone numbers
-const phoneNumbers = ["+1111-222-3333", "+1222-333-4444", "+1333-444-5555", "+1123-456-7890", "+1987-654-3210"];
+const phoneNumbers = ["111-222-3333", "222-333-4444", "333-444-5555", "123-456-7890", "987-654-3210"];
 
-phoneNumbers.forEach( phoneNumbers => console.log(phoneNumbers));
+phoneNumbers.forEach(function(numbers) {
+    console.log(numbers)
+
+    client.messages
+        .create({
+            to: '+1' + numbers,
+            from: process.env.TWILIO_NUMBER,
+            body: 'Do you prefer fries or tots?'
+        })
+        .then ((message) => console.log(message.sid))
+        .catch((err) => console.log(err));
+});
 
 
 
